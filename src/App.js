7c4bd3c046b84205e72f20586472bab3ff39149e
Shakib Farzan <farzan.shakib@gmail.com>
2021-12-17 00:24:20 +0330

    added routing

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
-import logo from "./logo.svg";
+import React from "react";
 import "./App.css";
+import { Switch, Route, Redirect } from "react-router-dom";
 import Navbar from "./components/navbar";
 import Homepage from "./components/homepage";
 import Footer from "./components/footer";
+import AboutPage from "./components/aboutPage";
+import BlogPage from "./components/blogPage";
+import ContactPage from "./components/contactPage";
+import NotFoundPage from "./components/notfoundPage";
+import ScrollToTop from "./components/common/scrollToTop";
 
 function App() {
   return (
@@ -11,7 +17,16 @@ function App() {
         <Navbar />
       </header>
       <main className="container">
-        <Homepage />
+        <Switch>
+          <Route path="/homepage" component={Homepage} />
+          <Route path="/about" component={AboutPage} />
+          <Route path="/contact" component={ContactPage} />
+          <Route path="/blog" component={BlogPage} />
+          <Route path="/not-found" component={NotFoundPage} />
+          <Redirect from="/" exact to={Homepage} />
+          <Redirect to="/homepage" />
+        </Switch>
+        <ScrollToTop />
       </main>
       <Footer />
     </>
